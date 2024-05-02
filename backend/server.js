2d8757065d6f5347db9af84327768de5b4b05848
João <joaosantalcantara@gmail.com>
2024-05-02 15:59:13 -0300

    test 07

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import { fastify } from "fastify";
 
 const server = fastify();
 
-server.get("/", async () => {
-  console.log("Server is running...")
-  return "Hello World!"
+// 500: INTERNAL_SERVER_ERROR
+// Code: FUNCTION_INVOCATION_FAILED
+// ID: gru1::pxqnb-1714675917018-ffb5d7c87161
+
+server.get("/", async (request, reply) => {
+  return { hello: "world" };
 });
 
 server.listen({
-  port: 3333,
+  port: 3000,
 })

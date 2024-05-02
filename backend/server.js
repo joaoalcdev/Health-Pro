334a5ccfc95a05061f840fcb15e5cd052a17de67
João <joaosantalcantara@gmail.com>
2024-05-02 15:44:14 -0300

    end setup

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import { fastify } from "fastify";
 
 const server = fastify();
 
-server.get("/", () => {
-  console.log("server is running...");
-  return "Hello World!";
-})
+server.get("/", async () => {
+  console.log("Server is running...")
+  return "Hello World!"
+});
 
 server.listen({
   port: 3333,

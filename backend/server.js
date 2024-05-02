91f700453563707adc47287294468836ec0c9cb6
João <joaosantalcantara@gmail.com>
2024-05-02 16:05:23 -0300

    test 09

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,13 @@
 import { fastify } from "fastify";
 
-const fastify = Fastify({
-  logger: true
-})
+const server = fastify();
 
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
 
-// Run the server!
-fastify.listen({ port: 3000 }, function (err) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+
+server.get("/", async (request, reply) => {
+  return "Hello World!";
+});
+
+server.listen({
+  port: 3333,
 })

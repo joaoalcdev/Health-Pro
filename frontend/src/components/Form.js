6b3854b698b3f80577d40e853543e0eb77c105e5
João <joaosantalcantara@gmail.com>
2024-05-22 23:20:58 -0300

    feat: removed unused lines

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -123,8 +123,6 @@ export function Select({ children, selectedPerson, setSelectedPerson, datas }) {
                 value={person}
                 disabled={person.unavailable}
               >
-                {/* {person.name ? `${person.name} (${person.UF})` : `${person.name}`} */}
-                {/* identify type person, if state, person.name+person.UF */}
                 {person.name} {person.UF && `(${person.UF})`} {person.type && `(${person.type})`}
               </Listbox.Option>
             ))}

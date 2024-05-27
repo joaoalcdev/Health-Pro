0dae9717916376d644f7e82da4afff950d395560
Mateus Gondim <55789472+MatsGonds@users.noreply.github.com>
2024-05-27 08:43:58 -0500

    Fix format phone number (#49)
    * Update professiona api integration
    
    * Fix replace error on formatPhone number util function
    
    * fix imports

--- frontend/src/utils/formatPhoneNumber.js
+++ frontend/src/utils/formatPhoneNumber.js
@@ -2,11 +2,14 @@
 export function formatPhoneNumber(number) {
   // Removendo caracteres não numéricos
 
+  if (!number) return '-';
   let phoneNumber = number.replace(/\D/g);
 
   // Verificando se o número tem 11 dígitos
   if (phoneNumber.length === 11) {
-    return `(${phoneNumber.slice(0, 2)}) ${phoneNumber.slice(2, 3)} ${phoneNumber.slice(3, 7)}-${phoneNumber.slice(7)}`;
+    const newNumber = `(${phoneNumber.slice(0, 2)}) ${phoneNumber.slice(2, 3)} ${phoneNumber.slice(3, 7)}-${phoneNumber.slice(7)}`
+
+    return newNumber;
   } else {
     return 'Número de telefone inválido.';
   }

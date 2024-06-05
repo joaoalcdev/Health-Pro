f661bfc45fb80abdde94107cc07196b38f1c657f
Mateus Gondim <mateusgondimon@gmail.com>
2024-06-05 07:48:16 -0500

    implement endpoint and logic to restore professional

--- backend/src/api/users/@usersRoutes.ts
+++ backend/src/api/users/@usersRoutes.ts
@@ -5,11 +5,13 @@ import { AddUser } from './addUser';
 import { UpdateUser } from './updateUser';
 import { DeleteUser } from './deleteUser';
 import { ViewUser } from './viewUser';
+import { RecoveryUser } from './recoveryUser';
 
 export const UsersRoutes =  async (app: FastifyInstance) => {
-  app.register(ListUsers)
-  app.register(AddUser)
-  app.register(UpdateUser)
-  app.register(DeleteUser)
-  app.register(ViewUser)
+  app.register(ListUsers);
+  app.register(AddUser);
+  app.register(UpdateUser);
+  app.register(DeleteUser);
+  app.register(ViewUser);
+  app.register(RecoveryUser);
 }; 

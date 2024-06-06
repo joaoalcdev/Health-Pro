2eaf0a23942d84f990d140292de10d33747b0663
Mateus Gondim <mateusgondimon@gmail.com>
2024-06-05 19:40:44 -0500

    remove logs

--- frontend/src/components/Tab.js
+++ frontend/src/components/Tab.js
@@ -3,9 +3,6 @@ import { on } from "rsuite/esm/DOMHelper";
 function Tab({ selectedTab, functions }) {
 
   const tabs = [{ name: "Ativos" }, { name: "Arquivados" }]
-  console.log(selectedTab)
-
-
 
   return (
     <div className="flex sm:w-[50%] xl:w-[30%] md:max-w-xl  rounded shadow">
@@ -13,14 +10,14 @@ function Tab({ selectedTab, functions }) {
         className={`w-full flex justify-center font-medium rounded-l px-5 py-2 border-t border-b ${selectedTab === 1 ? `bg-subMain text-white border-subMain hover:opacity-80` : ` bg-white text-gray-800  border-gray-200 hover:bg-gray-100`}`}
         onClick={() => functions.onChangeTab(1)}
       >
-        Ativos
+        {tabs[0].name}
       </button>
 
       <button aria-current="page"
         className={`w-full flex items-center gap-x-2 justify-center font-medium rounded-r px-5 py-2 border-t border-b ${selectedTab === 1 ? `bg-white text-gray-800  border-gray-200 hover:bg-gray-100` : `bg-subMain text-white border-subMain hover:opacity-80`}`}
         onClick={() => functions.onChangeTab(2)}
       >
-        Arquivados
+        {tabs[1].name}
       </button>
     </div>
 

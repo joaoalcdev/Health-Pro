4516a65d9b3d7671a776392e7465fd84b3112895
João Alcântara <joaosantalcantara@gmail.com>
2024-06-06 23:04:19 -0300

    refactor & feat: refactor and add new get on List Patients to show ddeleted patients

--- backend/src/api/patients/listPatients.ts
+++ backend/src/api/patients/listPatients.ts
@@ -1,22 +1,35 @@
-import {FastifyInstance, FastifyReply, FastifyRequest} from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { supabase } from "../../supabaseConnection";
 
 export const ListPatients = async (app: FastifyInstance) => {
-  app.get("/patients", async (req: FastifyRequest, res: FastifyReply) => {
+  app.get("/patients/:deleted", async (req: FastifyRequest, res: FastifyReply) => {
+    const { deleted } = req.params as { deleted: string }
     try {
-      const { data, error } = await supabase
-        .from("patients")
-        .select("*")
-        .order("fullName", { ascending: true })
-      
-      if (error) {
-        throw error
+      if (deleted === "true") {
+        let { data, error } = await supabase
+          .from("patients")
+          .select("*")
+          .not("deletedAt", "is", null)
+          .order("fullName", { ascending: true })
+        if (error) {
+          throw error
+        } else {
+          return res.status(200).send(data ? data : null)
+        }
       } else {
-        return res.status(200).send(data ? data : null)
+        const { data, error } = await supabase
+          .from("patients")
+          .select("*")
+          .filter("deletedAt", "is", null)
+          .order("fullName", { ascending: true })
+        if (error) {
+          throw error
+        } else {
+          return res.status(200).send(data ? data : null)
+        }
       }
     } catch (error) {
       return res.status(400).send(error)
     }
   })
-  
 }

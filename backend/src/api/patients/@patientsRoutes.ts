f3cc0f6ecc319d96e50872508c37889983a28185
João Alcântara <joaosantalcantara@gmail.com>
2024-06-06 22:59:22 -0300

    feat: added new endpoints of delete and restore to patients routes

--- backend/src/api/patients/@patientsRoutes.ts
+++ backend/src/api/patients/@patientsRoutes.ts
@@ -4,11 +4,14 @@ import { ListPatients } from './listPatients';
 import { AddPatient } from './addPatient';
 import { UpdatePatient } from './updatePatient';
 import { getPatient } from './getPatient';
-
+import { DeletePatient } from './deletePatient';
+import { RecoveryPatient } from './recoveryPatient';
 
 export const PatientsRoutes = async (app: FastifyInstance) => {
   app.register(ListPatients)
   app.register(AddPatient)
   app.register(UpdatePatient)
   app.register(getPatient)
+  app.register(DeletePatient)
+  app.register(RecoveryPatient)
 }; 

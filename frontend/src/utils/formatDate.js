c3fc0d2c8be3b2f59a0d6f7461756e52823e1f51
Mateus Gondim <mateusgondimon@gmail.com>
2024-06-10 20:32:44 -0500

    feat: new util to format time

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,7 +1,18 @@
 export function formatDate(date) {
+  date = new Date(date);
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
 
   return `${day}/${month}/${year}`;
+}
+
+
+export function formatDateTime(date) {
+  date = new Date(date);
+
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${hour}:${minutes}`;
 }

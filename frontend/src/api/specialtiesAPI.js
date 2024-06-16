e7129646fa66434102418590c88fb99789d5bac0
Mateus Gondim <mateusgondimon@gmail.com>
2024-06-16 14:27:09 -0500

    feat: api call

--- frontend/src/api/specialtiesAPI.js
+++ frontend/src/api/specialtiesAPI.js
@@ -12,3 +12,21 @@ export const getSpecialties = async () => {
     return error
   }
 }
+
+export const addSpecialties = async (data) => {
+  try {
+    const res = await axios.post(apiBaseUrl('specialties'), data)
+    return res
+  } catch (error) {
+    return error
+  }
+}
+
+export const updateSpecialties = async (id, data) => {
+  try {
+    const res = await axios.put(apiBaseUrl(`specialties/${id}`), data)
+    return res
+  } catch (error) {
+    return error
+  }
+}

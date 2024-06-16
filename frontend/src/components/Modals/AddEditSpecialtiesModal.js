fb4f4e97fe0bc259a7f358486d67d4053a6f4fe6
Mateus Gondim <mateusgondimon@gmail.com>
2024-06-16 14:26:13 -0500

    style: modal adjustments

--- frontend/src/components/Modals/AddEditSpecialtiesModal.js
+++ frontend/src/components/Modals/AddEditSpecialtiesModal.js
@@ -3,73 +3,125 @@ import Modal from './Modal';
 import { Button, Input, Switchi, Textarea } from '../Form';
 import { HiOutlineCheckCircle } from 'react-icons/hi';
 import { toast } from 'react-hot-toast';
+import { addSpecialties, updateSpecialties } from '../../api/specialtiesAPI';
+import { set } from 'rsuite/esm/utils/dateUtils';
 
-function AddEditSpecialtiesModal({ closeModal, isOpen, datas }) {
-  const [check, setCheck] = useState(false);
+function AddEditSpecialtiesModal({ closeModal, isOpen, datas, setStatus }) {
+  const [check, setCheck] = useState(true);
+  const [name, setName] = useState('');
+
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (datas?.name) {
-      setCheck(datas?.status);
+      setName(datas.name);
+      setCheck(datas.deletedAt ? false : true);
     }
   }, [datas]);
+
+  const handleSave = async () => {
+    setLoading(true);
+    if (name === '') {
+      return toast.error('Especialidade é obrigatória');
+    }
+    const response = await updateSpecialties(datas.id, { name, status: check });
+    if (response.code) {
+      if (response.response.data.code === "23505") {
+        toast.error('Especialidade com esse nome já existe');
+        setLoading(false);
+        return
+      }
+      toast.error('Erro ao atualizar especialidade');
+      setLoading(false);
+      return
+    }
+
+    setStatus(true);
+    toast.success('Especialidade atualizada com sucesso');
+    closeModal();
+    setLoading(false);
+  };
+
+  const handleAdd = async () => {
+    setLoading(true);
+    if (name === '') {
+      return toast.error('Especialidade é obrigatória');
+    }
+    const response = await addSpecialties({ name, status: check });
+    if (response.code) {
+      if (response.response.data.code === "23505") {
+        toast.error('Especialidade já existe');
+        setLoading(false);
+        return
+      }
+      toast.error('Erro ao adicionar especialidade');
+      setLoading(false);
+      return
+    }
+
+    setStatus(true);
+    toast.success('Especialidade adicionada com sucesso');
+    closeModal();
+    setLoading(false);
+  };
 
   return (
     <Modal
       closeModal={closeModal}
       isOpen={isOpen}
-      title={datas?.name ? 'Edit Service' : 'New Service'}
+      title={datas?.name ? 'Editar Especialidade' : 'Adicionar Especialidade'}
       width={'max-w-3xl'}
     >
-      <div className="flex-colo gap-6">
+      <div className="grid sm:grid-cols-2 gap-4 w-full">
         <Input
-          label="Service Name"
+          label="Especialidade"
           color={true}
-          placeholder={datas?.name && datas.name}
+          value={name}
+          placeholder={'Digite a especialidade'}
+          onChange={(e) => setName(e.target.value)}
         />
 
-        <Input
-          label="Price (Tsh)"
-          type="number"
-          color={true}
-          placeholder={datas?.price ? datas.price : 0}
-        />
-
-        {/* des */}
-        <Textarea
-          label="Description"
-          placeholder="Write description here..."
-          color={true}
-          rows={5}
-        />
         {/* switch */}
-        <div className="flex items-center gap-2 w-full">
+        <div className="flex items-center pt-8 gap-2 w-full">
           <Switchi
             label="Status"
             checked={check}
             onChange={() => setCheck(!check)}
           />
           <p className={`text-sm ${check ? 'text-subMain' : 'text-textGray'}`}>
-            {check ? 'Enabled' : 'Disabled'}
+            {check ? 'Ativado' : 'Desativado'}
           </p>
         </div>
         {/* buttones */}
-        <div className="grid sm:grid-cols-2 gap-4 w-full">
-          <button
-            onClick={closeModal}
-            className="bg-red-600 bg-opacity-5 text-red-600 text-sm p-4 rounded-lg font-light"
-          >
-            {datas?.name ? 'Discard' : 'Cancel'}
-          </button>
+        <button
+          onClick={closeModal}
+          className="bg-red-600 bg-opacity-5 text-red-600 text-sm p-4 rounded-lg font-light"
+        >
+          Cancelar
+        </button>
+        {datas?.name ?
           <Button
-            label="Save"
+            label="Salvar"
             Icon={HiOutlineCheckCircle}
+            loading={loading}
+            disabled={loading}
             onClick={() => {
-              toast.error('This feature is not available yet');
+              handleSave();
             }}
           />
-        </div>
+          :
+          <Button
+            label="Adicionar"
+            Icon={HiOutlineCheckCircle}
+            loading={loading}
+            disabled={loading}
+            onClick={() => {
+              handleAdd();
+            }}
+          />
+        }
       </div>
-    </Modal>
+    </Modal >
   );
 }
 

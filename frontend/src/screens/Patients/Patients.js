a353ed60d3fb3be6cd0b8c93f42fcfb893d686cd
João Alcântara <joaosantalcantara@gmail.com>
2024-06-20 22:46:29 -0300

    feat: added dynamic box values from api & all icons pattern & boxes animation & clean code

--- frontend/src/screens/Patients/Patients.js
+++ frontend/src/screens/Patients/Patients.js
@@ -1,99 +1,41 @@
+// dependencies - libraries
 import React, { useState, useEffect } from 'react';
-import Layout from '../../Layout';
-import { sortsDatas } from '../../components/Datas';
-import { Link, useNavigate } from 'react-router-dom';
-import { BiChevronDown, BiPlus, BiTime } from 'react-icons/bi';
-import { BsCalendarMonth } from 'react-icons/bs';
-import { MdFilterList, MdOutlineCalendarMonth } from 'react-icons/md';
 import { toast } from 'react-hot-toast';
-import { Button, FromToDate, Select } from '../../components/Form';
-import { PatientTable } from '../../components/Tables';
-import { BiLoaderCircle } from 'react-icons/bi';
-import ViewUserModal from '../../components/Modals/ViewUserModal';
 
-
-import AddPatientModal from '../../components/Modals/AddPatientModal';
-import { PatientsTable } from '../../components/Tables/PatientTable';
+// data - api
 import { deletePatient, getPatients, recoveryPatient } from '../../api/PatientsAPI';
-// import ViewUserModal from '../components/Modals/ViewUserModal';
-import ConfirmationModal from '../../components/Modals/ConfirmationModal';
-import Tab from '../../components/Tab';
 
+// icons
+import { HiOutlineTrash, HiMiniUserGroup, HiOutlineClock, HiOutlinePlus, HiMiniCalendarDays } from "react-icons/hi2";
+import { BiLoaderCircle } from 'react-icons/bi';
+
+// components
+import Layout from '../../Layout';
+import Tab from '../../components/Tab';
+import AddPatientModal from '../../components/Modals/AddPatientModal';
+import ConfirmationModal from '../../components/Modals/ConfirmationModal';
+import { useNavigate } from 'react-router-dom';
+import { PatientsTable } from '../../components/Tables/PatientTable';
 
 function Patients() {
   const navigate = useNavigate();
-  // const [gender, setGender] = useState(sortsDatas.genderFilter[0]);
-  // const [dateRange, setDateRange] = useState([new Date(), new Date()]);
-  // const [startDate, endDate] = dateRange;
 
-  // const sorts = [
-  //   {
-  //     id: 2,
-  //     selected: status,
-  //     setSelected: setStatus,
-  //     datas: sortsDatas.filterPatient,
-  //   },
-  //   {
-  //     id: 3,
-  //     selected: gender,
-  //     setSelected: setGender,
-  //     datas: sortsDatas.genderFilter,
-  //   },
-  // ];
-
-  // boxes
-  const boxes = [
-    {
-      id: 1,
-      title: 'Total Patients',
-      // value from api
-      value: '20',
-      color: ['bg-subMain', 'text-subMain'],
-      icon: BiTime,
-    },
-    {
-      id: 2,
-      title: 'Monthly Patients',
-      value: '230',
-      color: ['bg-orange-500', 'text-orange-500'],
-      icon: BsCalendarMonth,
-    },
-    {
-      id: 3,
-      title: 'Yearly Patients',
-      value: '1,500',
-      color: ['bg-green-500', 'text-green-500'],
-      icon: MdOutlineCalendarMonth,
-    },
-  ];
-
-  // preview
-  // const preview = (id) => {
-  //   navigate(`/patients/preview/${id}`);
-  // };
-
-
-
-  //data
+  // data
   const [allData, setAllData] = useState([])
   const [data, setData] = useState([]);
   const [patient, setPatient] = useState();
 
-  //controllers
+  // controllers
   const [isOpen, setIsOpen] = useState(false);
+  const [isAdd, setIsAdd] = useState(false);
+  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
   const [noData, setNoData] = useState(false);
   const [noResult, setNoResult] = useState(true);
   const [loading, setLoading] = useState(false);
   const [tab, setTab] = useState(1);
-  //controllers
   const [status, setStatus] = useState(false);
-  const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
 
-  const [isAdd, setIsAdd] = useState(false);
-
-
-  const [searchTerm, setSearchTerm] = useState("");
-
+  // api - get patients
   const fetch = async () => {
     setLoading(true)
     const response = await getPatients(tab === 1 ? '' : 'true')
@@ -110,11 +52,17 @@ function Patients() {
     setStatus(false)
   }
 
+  // dependencies
   useEffect(() => {
     fetch()
   }, [status, tab])
 
+  // api - preview patient
+  const preview = (id) => {
+    navigate(`/patients/preview/${id}`);
+  };
 
+  // api - delete patient
   const handleDelete = async () => {
     setLoading(true)
     const response = await deletePatient(patient)
@@ -131,26 +79,13 @@ function Patients() {
     }
   }
 
-  const onChangeTab = (index) => {
-    index === 1 ? setTab(1) : setTab(2);
-    setSearchTerm("");
-  }
-
-  const onCloseModal = () => {
-    setIsOpen(false);
-    setIsAdd(false);
-    setIsConfirmationOpen(false);
-  };
-
-  const preview = (id) => {
-    navigate(`/patients/preview/${id}`);
-  };
-
+  // api - remove patient (confirmation)
   const removePatient = (patient) => {
     setPatient(patient)
     setIsConfirmationOpen(true)
   };
 
+  // api - restore patient
   const restorePatient = async (patient) => {
     setLoading(true);
     const response = await recoveryPatient(patient);
@@ -163,6 +98,104 @@ function Patients() {
     }
   };
 
+  // modal controllers
+  const onCloseModal = () => {
+    setIsOpen(false);
+    setIsAdd(false);
+    setIsConfirmationOpen(false);
+  };
+
+  // search data
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // search cases
+  useEffect(() => {
+    setData(allData.filter((item) => {
+      // case 1 - no filter and no search
+      if (searchTerm === "") {
+        setNoResult(false)
+        return item
+      }
+      // case 2 - no filter but has search
+      if (item.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
+        setNoResult(false)
+        return item
+      }
+      // case 3 - no search but has filter
+      if (searchTerm === "") {
+        setNoResult(false)
+        return item
+      }
+      // case 4 - has filter and search
+      if (item.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
+        setNoResult(false)
+        return item
+      }
+    })
+    )
+  }, [searchTerm])
+
+  // data - no result
+  useEffect(() => {
+    if (data.length === 0) {
+      setNoResult(true)
+      return
+    }
+    setNoResult(false)
+  }, [data])
+
+  // tab controllers
+  const onChangeTab = (index) => {
+    index === 1 ? setTab(1) : setTab(2);
+    setSearchTerm("");
+  };
+
+  // boxes data - last30Days
+  const today = new Date();
+  const last30Days = new Date(today.setDate(today.getDate() - 30));
+  const valueOnRange = data.filter((item) => {
+    const date = new Date(item.createdAt);
+    return date >= last30Days;
+  });
+
+  const last30DaysPatients = valueOnRange.length;
+  // component - boxes data
+  const boxes = tab === 1 ? [
+    {
+      id: 1,
+      title: 'Pacientes Diários',
+      // value: [data.filter((item) => item.createdAt === today).length],
+      value: ['0'],
+      text: 'No Data :(',
+      color: ['bg-subMain', 'text-subMain'],
+      icon: HiOutlineClock,
+    },
+    {
+      id: 2,
+      title: 'Pacientes Mensais',
+      value: ['+', last30DaysPatients, ''],
+      text: 'novos pacientes',
+      color: ['bg-orange-500', 'text-orange-500'],
+      icon: HiMiniCalendarDays,
+    },
+    {
+      id: 3,
+      title: 'Pacientes Ativos (Total)',
+      value: ['', data.length, ' pacientes'],
+      text: 'ativos',
+      color: ['bg-green-500', 'text-green-500'],
+      icon: HiMiniUserGroup,
+    },
+  ] : [
+    {
+      id: 1,
+      title: 'Pacientes Arquivados (Total)',
+      value: data.length,
+      text: 'pacientes arquivados',
+      color: ['bg-red-500', 'text-red-500'],
+      icon: HiOutlineTrash,
+    },
+  ];
 
   // dynamic used patients
   const [dynamicUsed, setDynamicUsed] = useState(true);
@@ -178,41 +211,6 @@ function Patients() {
     handleResize();
     return () => window.removeEventListener('resize', handleResize);
   }, []);
-
-
-  useEffect(() => {
-    setData(allData.filter((item) => {
-      //case 1 - no filter and no search
-      if (searchTerm === "") {
-        setNoResult(false)
-        return item
-      }
-      //case 2 - no filter but has search
-      if (item.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
-        setNoResult(false)
-        return item
-      }
-      //case 3 - no search but has filter
-      if (searchTerm === "") {
-        setNoResult(false)
-        return item
-      }
-      //case 4 - has filter and search
-      if (item.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
-        setNoResult(false)
-        return item
-      }
-    })
-    )
-  }, [searchTerm])
-
-  useEffect(() => {
-    if (data.length === 0) {
-      setNoResult(true)
-      return
-    }
-    setNoResult(false)
-  }, [data])
 
   return (
     <Layout>
@@ -249,7 +247,7 @@ function Patients() {
         }}
         className="w-16 animate-bounce h-16 border border-border z-50 bg-subMain text-white rounded-full flex-colo fixed bottom-8 right-12 button-fb"
       >
-        <BiPlus className="text-2xl" />
+        <HiOutlinePlus className="text-2xl" />
       </button>
       <div className="sm:flex grid grid-cols-1 gap-4 items-center justify-between">
         <h1 className="text-xl font-semibold">Pacientes</h1>
@@ -258,34 +256,42 @@ function Patients() {
           functions={{ onChangeTab }}
         />
       </div>
-      {/* <h1 className="text-xl font-semibold">Pacientes</h1> */}
       {/* boxes */}
       {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
           {boxes.map((box) => (
             <div
-              key={box.id}
-              className="bg-white flex-btn gap-4 rounded-xl border-[1px] border-border p-5"
+              data-aos="fade-left"
+              data-aos-duration="1000"
+              data-aos-delay="10"
+              data-aos-offset="100"
+              data-aos-easing="ease"
+              data-aos-once="true"
+              className=""
             >
-              <div className="w-3/4">
-                <h2 className="text-sm font-medium">{box.title}</h2>
-                <h2 className="text-xl my-6 font-medium">{box.value}</h2>
-                <p className="text-xs text-textGray">
-                  Total Patients
-                  <span className={box.color[1]}>{box.value}</span>{' '}
-                  {
-                    box.title === 'Total Patients'
-                      ? 'today'
-                      : box.title === 'Monthly Patients'
-                        ? 'this month'
-                        : 'this year'
-                  }
-                </p>
-              </div>
               <div
-                className={`w-10 h-10 flex-colo rounded-md text-white text-md ${box.color[0]}`}
+                key={box.id}
+                className="select-text bg-white flex-btn gap-4 rounded-xl border-[1px] border-border p-5 hover:-translate-y-2 hover:shadow-md transition-all duration-300 ease-in-out"
               >
-                <box.icon />
+                <div className="w-3/4">
+                  <h2 className="text-sm font-medium">{box.title}</h2>
+                  <h2 className="text-xl my-6 font-medium">{box.value}</h2>
+                  <p className="text-xs text-textGray">
+                    <span className={box.color[1]}> {box.value} {box.text}</span>{' '}
+                    {
+                      box.title === 'Pacientes Diários'
+                        ? ''
+                        : box.title === 'Pacientes Mensais'
+                          ? 'esse mês'
+                          : 'na clínica'
+                    }
+                  </p>
+                </div>
+                <div
+                  className={`w-10 h-10 flex-colo rounded-md text-white text-md ${box.color[0]}`}
+                >
+                  <box.icon />
+                </div>
               </div>
             </div>
           ))}
@@ -314,33 +320,11 @@ function Patients() {
                 }}
                 className="h-14 w-full text-sm text-main rounded-md bg-dry border border-border px-4"
               />
-              {/* sort  */}
-              {/* {sorts.map((item) => (
-                <Select
-                  key={item.id}
-                  selectedPerson={item.selected}
-                  setSelectedPerson={item.setSelected}
-                  datas={item.datas}
-                >
-                  <div className="h-14 w-full text-xs text-main rounded-md bg-dry border border-border px-4 flex items-center justify-between">
-                    <p>{item.selected.name}</p>
-                    <BiChevronDown className="text-xl" />
-                  </div>
-                </Select>
-              ))} */}
-              {/* date */}
-              {/* <FromToDate
-                startDate={startDate}
-                endDate={endDate}
-                bg="bg-dry"
-                onChange={(update) => setDateRange(update)}
-              /> */}
-              {/* export */}
               {/* <Button
-                label="Filter"
+                label="Filtrar"
                 Icon={MdFilterList}
                 onClick={() => {
-                  toast.error('Filter data is not available yet');
+                  toast.error('Filtros não disponíveis no momento', { position: 'top-center' });
                 }}
               /> */}
             </div>

6bdfd5d1717f4a699073733aa4d7be0217c2ed23
Mateus Gondim <mateusgondimon@gmail.com>
2024-07-16 23:26:54 -0300

    feat: checkIn endpoint

--- backend/src/api/events/eventCheckIn.ts
+++ backend/src/api/events/eventCheckIn.ts
@@ -1,39 +1,62 @@
 import {FastifyInstance, FastifyReply, FastifyRequest} from 'fastify';
 import { supabase } from "../../supabaseConnection";
 import moment from 'moment-timezone';
+import Jimp from 'jimp';
 
 import 'moment/locale/pt-br';
 
 export const EventCheckIn = async (app: FastifyInstance) => {
-  app.put("/checkIn/:id", async (req: FastifyRequest, res: FastifyReply) => {
-    try{
-      const {id} = req.params as {id: string}
-      const {
-        checkInName
-      } = req.body as ScheduleEvent;
+  app.post("/checkIn/:id", async (req: FastifyRequest, res: FastifyReply) => {
+    const { id } = req.params as { id: string };  
+    let {
+      checkInName,
+      checkInSignature,
+    } = req.body as {checkInName: string, checkInSignature: string};
 
-      const {data, error} = await supabase
-        .from('eventInstances')
-        .update({
-          checkInName,
-          checkInDate: moment.tz(new Date(), "America/Fortaleza"),
-          eventStatus: 3
-        })
-        .eq('id', id)
-        .select()
-      
-      if(error){
-        throw error
-      }else{
-        if(data){
-          res.status(200).send({data})
-        }else{
-          res.status(404).send({error: 'Event not found'})
-        }
+    //Convert base64 to buffer
+    const buffer = Buffer.from(checkInSignature, "base64");
+    Jimp.read(buffer, (err, res) => {
+      if (err) throw new Error(err.message);
+      res.quality(5).write("resized.jpg");
+    });
+    
+    try {
+      //Upload signature to storage
+      const { data: fileData, error } = await supabase
+      .storage
+      .from('cedejom')
+      .upload(`signatures/sign${id}.png`, buffer as Buffer , { 
+        cacheControl: '3600', 
+        upsert: false, 
+        contentType: "image/png",
+      });
+
+      if (error) {
+        return res.status(500).send({ error });
       }
-    }
-    catch(error){
-      res.status(500).send(error)
+
+      //Update event instance
+      const { data, error: insertError } = await supabase
+      .from('eventInstances')
+      .update({
+        checkInName,
+        checkInSignature: fileData.path,
+        checkInDate: moment().tz('America/Sao_Paulo').format(),
+        eventStatus: 3,
+      })
+      .eq('id', id)
+      .select()
+
+      if (insertError) {
+        return res.status(500).send({ error: insertError });
+      }
+
+      return res.status(200).send({data});
+  
+    } catch (error) {
+      res.send({ error });
     }
   })
+
 }
+

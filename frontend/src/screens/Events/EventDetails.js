15d9ff4d32b745fdd2f574f121d4c1d057c15082
João Alcântara <joaosantalcantara@gmail.com>
2024-10-03 19:09:50 -0300

    feat: added new data to EventDetailsPatientInfo / EventDeailtsInfo / EventoDetailsProfessionalInfo

--- frontend/src/screens/Events/EventDetails.js
+++ frontend/src/screens/Events/EventDetails.js
@@ -50,9 +50,9 @@ function EventDetails() {
       case 1:
         return <EventDetailsInfo data={eventData} onStatus={onStatus} />;
       case 2:
-        return <EventDetailsPatientInfo />;
+        return <EventDetailsPatientInfo data={eventData} onStatus={onStatus} />;
       case 3:
-        return <EventDetailsProfessionalInfo doctor={true} />;
+        return <EventDetailsProfessionalInfo data={eventData} doctor={true} onStatus={onStatus} />;
       case 4:
         return <EventDetailsReminder doctor={true} />;
       default:
@@ -60,90 +60,89 @@ function EventDetails() {
     }
   };
 
-  return (
-    loading ?
-      <div className="flex absolute items-center justify-center w-full h-1/2">
-        <BiLoaderCircle className="animate-spin text-subMain text-2xl" />
+  return (loading ?
+    <div className="flex absolute items-center justify-center w-full h-1/2">
+      <BiLoaderCircle className="animate-spin text-subMain text-2xl" />
+    </div>
+    :
+    <Layout>
+      <div className="flex items-center gap-4">
+        <Link
+          to="/schedule"
+          className="bg-white border border-subMain border-dashed rounded-lg py-3 px-4 text-md"
+        >
+          <IoArrowBackOutline />
+        </Link>
+        <h1 className="text-xl font-semibold">
+          {
+            eventData.eventType === 4 ? 'Consulta' :
+              eventData.eventType === 5 ? 'Retorno' :
+                eventData.serviceName}
+        </h1>
       </div>
-      :
-      <Layout>
-        <div className="flex items-center gap-4">
-          <Link
-            to="/schedule"
-            className="bg-white border border-subMain border-dashed rounded-lg py-3 px-4 text-md"
-          >
-            <IoArrowBackOutline />
-          </Link>
-          <h1 className="text-xl font-semibold">
-            {
-              eventData.eventType === 4 ? 'Consulta' :
-                eventData.eventType === 5 ? 'Retorno' :
-                  eventData.serviceName}
-          </h1>
-        </div>
-        <div className=" grid grid-cols-12 gap-6 my-8 items-start">
-          <div
-            data-aos="fade-right"
-            data-aos-duration="1000"
-            data-aos-delay="100"
-            data-aos-offset="200"
-            className="col-span-12 flex-colo gap-6 lg:col-span-3 bg-white rounded-xl border-[1px] border-border p-6 lg:sticky top-28"
-          >
+      <div className=" grid grid-cols-12 gap-6 my-8 items-start">
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+          data-aos-offset="200"
+          className="col-span-12 flex-colo gap-6 lg:col-span-3 bg-white rounded-xl border-[1px] border-border p-6 lg:sticky top-28"
+        >
 
-            <div className="gap-4 flex-colo w-full">
-              <div className='flex-colo h-full w-full max-w-[300px] bg-subMain rounded-lg p-1'>
-                <div className='flex-colo w-full border rounded-lg justify-center p-4'>
-                  <h1 className='text-md text-white uppercase'>
-                    {moment(eventData.startTime).calendar().split(' ')[0] === 'Ontem' ? 'Ontem' :
-                      moment(eventData.startTime).calendar().split(' ')[0] === 'Hoje' ? 'Hoje' : moment(eventData.startTime).calendar().split(' ')[0] === 'Amanhã' ? 'Amanhã' : moment(eventData.startTime).format('dddd')}
-                  </h1>
-                  <h1 className='text-[100px] leading-[100px] text-white font-bold  '>
-                    {new Date(eventData.startTime).getDate() < 10 ? '0' + new Date(eventData.startTime).getDate() : new Date(eventData.startTime).getDate().toString()}
-                  </h1>
+          <div className="gap-4 flex-colo w-full">
+            <div className='flex-colo h-full w-full max-w-[300px] bg-subMain rounded-lg p-1'>
+              <div className='flex-colo w-full border rounded-lg justify-center p-4'>
+                <h1 className='text-md text-white uppercase'>
+                  {moment(eventData.startTime).calendar().split(' ')[0] === 'Ontem' ? 'Ontem' :
+                    moment(eventData.startTime).calendar().split(' ')[0] === 'Hoje' ? 'Hoje' : moment(eventData.startTime).calendar().split(' ')[0] === 'Amanhã' ? 'Amanhã' : moment(eventData.startTime).format('dddd')}
+                </h1>
+                <h1 className='text-[100px] leading-[100px] text-white font-bold  '>
+                  {new Date(eventData.startTime).getDate() < 10 ? '0' + new Date(eventData.startTime).getDate() : new Date(eventData.startTime).getDate().toString()}
+                </h1>
 
-                  <div className='flex'>
-                    <h1 className='text-sm text-white p-2 text-right uppercase'>
-                      {moment(eventData.startTime).format('MMMM/YYYY')}
-                    </h1>
-                  </div>
-                  <h1 className='text-[30px] text-white font-bold'>
-                    {moment(eventData.startTime).format('h:mm A')}
+                <div className='flex'>
+                  <h1 className='text-sm text-white p-2 text-right uppercase'>
+                    {moment(eventData.startTime).format('MMMM/YYYY')}
                   </h1>
-
                 </div>
+                <h1 className='text-[30px] text-white font-bold'>
+                  {moment(eventData.startTime).format('h:mm A')}
+                </h1>
+
               </div>
             </div>
-            {/* tabs */}
-            <div className="flex-colo gap-3 w-full">
-              {eventsTab.map((tab, index) => (
-                <button
-                  onClick={() => setActiveTab(tab.id)}
-                  key={index}
-                  className={`
+          </div>
+          {/* tabs */}
+          <div className="flex-colo gap-3 w-full">
+            {eventsTab.map((tab, index) => (
+              <button
+                onClick={() => setActiveTab(tab.id)}
+                key={index}
+                className={`
                 ${activeTab === tab.id
-                      ? 'bg-text text-subMain text-left text-balance '
-                      : 'bg-dry text-main hover:bg-text hover:text-subMain text-left text-balance'
-                    }
+                    ? 'bg-text text-subMain text-left text-balance '
+                    : 'bg-dry text-main hover:bg-text hover:text-subMain text-left text-balance'
+                  }
                 text-xs gap-4 flex items-center w-full p-4 rounded`}
-                >
-                  <tab.icon className="text-lg" /> {tab.id === 2 ? eventData.patientFullName : tab.id === 3 ? eventData.professionalFirstName + ' ' + eventData.professionalLastName : tab.title}
-                </button>
-              ))}
-            </div>
+              >
+                <tab.icon className="text-lg" /> {tab.id === 2 ? eventData.patientFullName : tab.id === 3 ? eventData.professionalFirstName + ' ' + eventData.professionalLastName : tab.title}
+              </button>
+            ))}
+          </div>
 
-          </div>
-          {/* tab panel */}
-          <div
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            data-aos-delay="100"
-            data-aos-offset="200"
-            className="col-span-12 lg:col-span-9 bg-white rounded-xl border-[1px] border-border p-6"
-          >
-            {tabPanel()}
-          </div>
         </div>
-      </Layout>
+        {/* tab panel */}
+        <div
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+          data-aos-offset="200"
+          className="col-span-12 lg:col-span-9 bg-white rounded-xl border-[1px] border-border p-6"
+        >
+          {tabPanel()}
+        </div>
+      </div>
+    </Layout>
   );
 }
 

edd409ee440eee797bf79235580644d42e941fea
João Alcântara <joaosantalcantara@gmail.com>
2024-10-21 15:44:10 -0300

    feat: Created & Added TabSwitch with Contents

--- frontend/src/screens/Events/EventDetailsPatientInfo.js
+++ frontend/src/screens/Events/EventDetailsPatientInfo.js
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState, useEffect, Fragment } from 'react';
+import clsx from 'clsx';
 
 // components - import
 import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 
 // datas - import
@@ -16,11 +19,14 @@ import { BiChevronDown } from 'react-icons/bi';
 import { TbUserHeart } from "react-icons/tb";
 import { LiaGenderlessSolid } from "react-icons/lia";
 import { LiaTintSolid } from "react-icons/lia";
+import { HiEllipsisVertical, HiMiniCalendarDays } from "react-icons/hi2";
+
 import { HiOutlinePhone, HiOutlineCalendarDays, HiOutlineIdentification, HiOutlineMapPin, HiOutlineCheckCircle, HiMiniPencilSquare, HiArrowLeft, HiOutlineHome, HiMiniFingerPrint, HiOutlineCreditCard, HiOutlineHomeModern } from 'react-icons/hi2';
 
 
 function EventDetailsPatientInfo({ data, onStatus }) {
   const nav = useNavigate();
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const [gender, setGender] = useState(genderDatas.gender[data.patientGender - 1]);
   const { id } = useParams();
@@ -53,10 +59,8 @@ function EventDetailsPatientInfo({ data, onStatus }) {
               className="flex items-center gap-2 px-6 py-2 border border-subMain hover:border-main rounded-md text-subMain hover:text-main transitionss"
               onClick={handleViewPatientsDetails}
             >
-              {/* <Link to={handleViewPatientsDetails} className="flex items-center gap-2 px-6 py-2 border border-subMain hover:border-main rounded-md text-subMain hover:text-main transitions"> */}
               <HiMiniPencilSquare className="text-xl" />
               <p className="font-normal">Ver detalhes</p>
-              {/* </Link> */}
             </button>
           </div>
         </div>
@@ -140,9 +144,164 @@ function EventDetailsPatientInfo({ data, onStatus }) {
           </div>
         </div>
       </div>
+      {/* divider */}
+      <div class="flex w-full items-center rounded-full">
+        <div class="flex-1 border-b border-gray-300"></div>
+        <span class="text-gray-500 text-md font-medium leading-8 px-8 py-6">agenda</span>
+        <div class="flex-1 border-b border-gray-300"></div>
+      </div>
       {/* table */}
-      
-      {/* table */}
+      <TabGroup
+        selectedIndex={selectedIndex} onChange={setSelectedIndex}
+      >
+        <TabList className="flex flex-col w-full justify-end md:flex-row md:w-full items-center gap-4 py-3">
+          <div className='flex flex-col w-full justify-start md:flex-row md:w-full items-center'>
+            <h1 className='flex flex-col font-semibold text-2xl text-black text-center md:text-left w-full text-wrap'>{selectedIndex ? "Agendamentos futuros" : "Histórico de Agendamentos"}</h1>
+          </div>
+          <div className='flex flex-col sm:flex-row gap-1 md:gap-2'>
+            <Tab as={Fragment}>
+              {({ hover, selected }) => (
+                <button
+                  className={clsx("flex w-full justify-center items-center gap-1 px-4 py-1 md:gap-2 md:px-6 md:py-2 border border-subMain text-subMain bg-gray-100 hover:border-main hover:text-main hover:bg-white rounded-md text-nowrap transitions", hover && "", selected && "bg-subMain text-white")}
+                >
+                  <HiMiniPencilSquare className="text-xl" />
+                  <p className="font-normal text-center">Histórico</p>
+                </button>
+              )}
+            </Tab>
+            <Tab as={Fragment}>
+              {({ hover, selected }) => (
+                <button
+                  className={clsx("flex w-full justify-center items-center gap-1 px-4 py-1 md:gap-2 md:px-6 md:py-2 border border-subMain text-subMain bg-gray-100 hover:border-main hover:text-main hover:bg-white rounded-md text-nowrap transitions", hover && "", selected && "bg-subMain text-white")}
+                >
+                  <HiMiniCalendarDays className="text-xl" />
+                  <p className="font-normal text-center">Proximas consultas</p>
+                </button>
+              )}
+            </Tab>
+          </div>
+        </TabList>
+        <TabPanels>
+          {data.lenght > 0 && data.map((item, index) => (
+            <TabPanel className={`flex flex-col w-full justify-center items-center space-y-2`}>
+              {/* <div className="flex flex-col w-full justify-center items-center space-y-2"> */}
+              <div key={item} className="flex w-full justify-center items-center bg-gray-200 hover:bg-gray-100 hover:translate-y-[-0.5rem] rounded-lg p-4 group transitions">
+                <div className="w-full flex justify-center items-center">
+                  {/* N */}
+                  <span className="flex flex-col justify-center items-center ">
+                    <p className='flex justify-center items-center text-center bg-subMain size-9 group-hover:bg-opacity-85 text-white rounded-lg'>{(index + 1)}</p>
+                  </span>
+                  {/* Paciente / Nome */}
+                  <div className="grid grid-cols-3 sm:grid-cols-3 gap-4 w-full justify-items-center">
+                    {/* <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      <p className='flex text-sm text-black'>Paciente</p>
+                      <p className='flex text-xs text-gray-500'>João Alcântara</p>
+                    </div> */}
+                    {/* Serviço / Profissional */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      <p className='flex text-sm text-black'>Serviço</p>
+                      <p className='flex text-xs text-gray-500'>Profissional</p>
+                    </div>
+                    {/* Tipo de evento */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      {/* <p className='flex text-sm text-black'>Tipo de evento</p> */}
+                      <p className='flex text-sm text-gray-500'>Evento x</p>
+                    </div>
+                    {/* Data */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      {/* <p className='flex text-sm text-black'>Data</p> */}
+                      <p className='flex text-sm text-gray-500'>__/__/____</p>
+                    </div>
+                    {/* Config */}
+                  </div>
+                  <div className="flex flex-col justify-center items-center">
+                    <button className="flex size-9 justify-center items-center rounded-lg">
+                      <h3><HiEllipsisVertical className="text-2xl text-black" /></h3>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* </div> */}
+            </TabPanel>
+          ))}
+          <TabPanel>
+            <div className="flex flex-col w-full justify-center items-center space-y-2">
+              <div className="flex w-full justify-center items-center bg-gray-200 rounded-lg p-4 group">
+                <div className="w-full flex justify-center items-center">
+                  {/* N */}
+                  <span className="flex flex-col justify-center items-center ">
+                    <p className='flex justify-center items-center text-center bg-subMain size-9 group-hover:bg-opacity-85 text-white rounded-lg'>01</p>
+                  </span>
+                  {/* Paciente / Nome */}
+                  <div className="grid grid-cols-3 sm:grid-cols-3 gap-4 w-full justify-items-center">
+                    {/* <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      <p className='flex text-sm text-black'>Paciente</p>
+                      <p className='flex text-xs text-gray-500'>João Alcântara</p>
+                    </div> */}
+                    {/* Serviço / Profissional */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start order-first">
+                      <p className='flex text-sm text-black'>Serviço</p>
+                      <p className='flex text-xs text-gray-500'>Profissional</p>
+                    </div>
+                    {/* Tipo de evento */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start order-last sm:order-3">
+                      {/* <p className='flex text-sm text-black'>Tipo de evento</p> */}
+                      <p className='flex text-sm text-gray-500'>Evento x</p>
+                    </div>
+                    {/* Data */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start order-2 sm:order-2">
+                      {/* <p className='flex text-sm text-black'>Data</p> */}
+                      <p className='flex text-sm text-gray-500'>__/__/____</p>
+                    </div>
+                    {/* Config */}
+                  </div>
+                  <div className="flex flex-col justify-center items-center">
+                    <button className="flex size-9 justify-center items-center rounded-lg">
+                      <h3><HiEllipsisVertical className="text-2xl text-black" /></h3>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full justify-center items-center bg-gray-200 rounded-lg p-4 group">
+                <div className="w-full flex justify-center items-center">
+                  {/* N */}
+                  <span className="flex flex-col justify-center items-center ">
+                    <p className='flex justify-center items-center text-center bg-subMain size-9 group-hover:bg-opacity-85 text-white rounded-lg'>01</p>
+                  </span>
+                  {/* Paciente / Nome */}
+                  <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 w-full justify-items-center">
+                    {/* <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      <p className='flex text-sm text-black'>Paciente</p>
+                      <p className='flex text-xs text-gray-500'>João Alcântara</p>
+                    </div> */}
+                    {/* Serviço / Profissional */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      <p className='flex text-sm text-black'>Serviço</p>
+                      <p className='flex text-xs text-gray-500'>Profissional</p>
+                    </div>
+                    {/* Tipo de evento */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      {/* <p className='flex text-sm text-black'>Tipo de evento</p> */}
+                      <p className='flex text-sm text-gray-500'>Evento x</p>
+                    </div>
+                    {/* Data */}
+                    <div className="flex text-center flex-col justify-center items-center sm:items-start">
+                      {/* <p className='flex text-sm text-black'>Data</p> */}
+                      <p className='flex text-sm text-gray-500'>__/__/____</p>
+                    </div>
+                    {/* Config */}
+                  </div>
+                  <div className="flex flex-col justify-center items-center">
+                    <button className="flex size-9 justify-center items-center rounded-lg">
+                      <h3><HiEllipsisVertical className="text-2xl text-black" /></h3>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
     </>
   );
 }

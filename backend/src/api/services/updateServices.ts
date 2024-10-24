a3c51fbff91f8affa455e2b7044bb256825d07f0
Mateus Gondim <mateusgondimon@gmail.com>
2024-10-23 23:41:40 -0300

    refactor-update-services

--- backend/src/api/services/updateServices.ts
+++ backend/src/api/services/updateServices.ts
@@ -10,28 +10,70 @@ export const UpdateService = async (app: FastifyInstance) => {
         name,
         status,
         specialtyId,
-        initialPrice,
-        recurringPrice,
-      } = req.body as Service
+        prices, 
+      } = req.body as Service & { prices: Price[] }	
 
       const { data, error } = await supabase
       .from("services")
       .update([{
         name,
-        specialtyId,
-        initialPrice,
-        recurringPrice,
         deletedAt: status === true ? null : new Date(), 
       }])
       .eq("id", id)
       .select()
 
-      if (error) {
-        throw error
-      } 
-        else {
-          return res.status(200).send(data ? data : null)
+      const { data: servicePrices, error: servicePricesError } = await supabase
+      .from("servicePrices")
+      .select('*')
+      .eq("serviceId", id)
+
+      if ( servicePricesError) {
+        throw servicePricesError
+      }
+
+      let addPrices = [] as Price[]
+      let updatedPrices = [] as Price[]
+
+      if(servicePrices && servicePrices.length > 0) {
+        prices.forEach(async (price: Price) => {
+          const priceIndex = servicePrices.findIndex((servicePrice) => servicePrice.agreementId === price.agreementId)
+          if(priceIndex >= 0) {
+            await supabase
+            .from("servicePrices")
+            .update({
+              price: price.price, 
+            })
+            .eq("id", servicePrices[priceIndex].id)
+          }else {
+            addPrices.push({ 
+              specialtyId: specialtyId,
+              serviceId: Number(id),
+              price: price.price,
+              agreementId: price.agreementId, 
+            })
+          }
+        })
+      }else {
+        prices.forEach((price: Price) => {
+          addPrices.push({ 
+            specialtyId: specialtyId,
+            serviceId: Number(id),
+            price: price.price,
+            agreementId: price.agreementId, 
+          })
+        })
+      }
+      if(addPrices.length > 0) {
+        const { data: servicePrices, error: servicePricesError } = await supabase
+        .from("servicePrices")
+        .insert(addPrices)
+        .select()
+        if (servicePricesError) {
+          throw servicePricesError
         }
+      }
+      return res.status(200).send(data ? data : null)
+
     } catch (error) {
       return res.status(400).send(error)
     }

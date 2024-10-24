55b0fb29617fe7a75f62a473c512e8a0191335f9
Mateus Gondim <mateusgondimon@gmail.com>
2024-10-23 23:41:04 -0300

    refactor-list-specialties

--- backend/src/api/specialties/listSpecialties.ts
+++ backend/src/api/specialties/listSpecialties.ts
@@ -26,6 +26,35 @@ export const ListSpecialties = async (app: FastifyInstance) => {
           .from("specialties")
           .select("*")
           .order("name")
+
+        let { data: services, error: errorServices } = await supabase
+          .from("services")
+          .select("*")
+          .is("deletedAt", null)
+          .order("id")
+
+        let {data: servicePrices, error: errorServicePrices} = await supabase
+          .from("view_service_prices")
+          .select("*")
+          .order("agreementId")
+
+        let { data: regularPrices, error: errorRegularPrices } = await supabase
+          .from("view_regular_prices")
+          .select("*")
+          .order("agreementId")
+
+          if(data && services && regularPrices && servicePrices) {
+            const serviceData = [...services]
+            serviceData.forEach((service) => {
+              service.prices = servicePrices.filter((servicePrice) => servicePrice.serviceId === service.id)
+            })
+            const responseData =[...data]
+            responseData.forEach((specialty) => {
+              specialty.services = serviceData.filter((service) => service.specialtyId === specialty.id)
+              specialty.regularPrices = regularPrices.filter((regularPrice) => regularPrice.specialtyId === specialty.id)
+            })
+            data = responseData
+          }
         
         if (error) {
           throw error

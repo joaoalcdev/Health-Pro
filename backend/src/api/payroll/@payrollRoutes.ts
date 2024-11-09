eaa29cfcd0b1e609398d15e071f2270b3f1382d1
Mateus Gondim <mateusgondimon@gmail.com>
2024-11-08 20:28:13 -0600

    feat-export-payroll

--- backend/src/api/payroll/@payrollRoutes.ts
+++ backend/src/api/payroll/@payrollRoutes.ts
@@ -1,9 +1,11 @@
 import {FastifyInstance} from 'fastify';
 import { getPayroll } from './getPayroll';
 import { EditPayroll } from './editPayroll';
+import { exportPayroll } from './exportPayroll';
 
 
 export const PayrollRoutes =  async (app: FastifyInstance) => {
  app.register(getPayroll);
  app.register(EditPayroll);
+  app.register(exportPayroll);
 }
